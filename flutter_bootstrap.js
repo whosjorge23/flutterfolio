64aa843d5c8c52ec2fa39f29a76d4f23cc9dbe43
Giorgio Giannotta <jorge_upa@msn.com>
2025-07-12 08:50:07 +0200

    second build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"18818009497c581ede5d8a3b8b833b81d00ceb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1903146610"
+    serviceWorkerVersion: "2607778185"
   }
 });
