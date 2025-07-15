b572d60be2fc513940543da57e680369150c9d2e
Giorgio Giannotta <jorge_upa@msn.com>
2025-07-15 19:47:15 +0200

    fixes models

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"18818009497c581ede5d8a3b8b833b81d00ceb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1122525475"
+    serviceWorkerVersion: "4074921009"
   }
 });
